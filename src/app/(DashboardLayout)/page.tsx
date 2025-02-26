f4a9bf03e7d8ff0940a5afdc9b15de137e9cbd6d
Glory <okewoleglory@gmail.com>
2025-02-26 11:50:11 +0100

    feat: implemented invoice table

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CardBox from "../components/shared/CardBox";
+import PaginationTable from "../components/resuable/Pagination";
 
 const page = () => {
   return (
@@ -12,6 +13,28 @@ const page = () => {
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s
         </p>
+        <PaginationTable tableData={[
+            {
+              customer: "/images/blog/blog-img2.jpg",
+              customer_name: "Popular Authors",
+              handle: "Most Successful",
+              tranche: "40%",
+              amount: '$9000',
+              status: "pending",
+              product_name: "awaHealth",
+              service: ['Brand Design, UI/Ux, Web dev'],
+            },
+            {
+              customer: "/images/blog/blog-img2.jpg",
+              customer_name: "Popular Authors",
+              handle: "Most Successful",
+              tranche: "60%",
+              amount: '$20000',
+              status: "paid",
+              product_name: "Venture Nation",
+              service: ['Brand Design, Web Dev'],
+            },
+        ]} />
         
       </CardBox>
     </>
